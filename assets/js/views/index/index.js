5e5cdc59ff570fdbfd1b99a8cf014397c1486802
nuintun <nuintun@qq.com>
2016-08-18 17:50:00 +0800

    update files

--- assets/js/views/index/index.js
+++ assets/js/views/index/index.js
@@ -5,8 +5,23 @@
 'use strict';
 
 var $ = require('jquery');
+var Lazy = require('lazy');
 var Toast = require('toast');
 
 $(function (){
-  
+  var array = [1, 2, 3, 4, 5, 6];
+  var lazyArray = Lazy([1, 2, 3, 4, 5, 6])
+    .filter(function (item){
+      seajs.log('filter');
+
+      return item < 4;
+    })
+    .map(function (item){
+      seajs.log('map');
+
+      return item * item;
+    });
+
+  seajs.log(array);
+  seajs.log(lazyArray.value());
 });
