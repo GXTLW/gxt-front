f56ec5e5fd9df0e67803388c8dbf7a49a594229d
nuintun <nuintun@163.com>
2016-09-17 20:50:51 +0800

    update files

---
+++ deploy.js
@@ -0,0 +1,50 @@
+/**
+ * Created by liuyan on 2016/9/17.
+ */
+
+var fs = require('fs');
+var path = require('path');
+var http = require('http');
+var rimraf = require('del');
+var safeWriteStream = require('safe-write-stream');
+
+rimraf.sync('deplay');
+
+function normalize(src){
+  return '/' + src.replace(/\\/g, '/');
+}
+
+function request(src, callback){
+  return http.get('http://127.0.0.1:9090' + src, callback);
+}
+
+function read(dir){
+  fs.readdir(dir, function (error, files){
+    if (error) throw error;
+
+    files.forEach(function (filename){
+      var src = path.join(dir, filename);
+
+      fs.stat(src, function (error, stats){
+        if (stats.isDirectory()) {
+          read(src);
+        } else {
+          src = normalize(src);
+
+          request(src, function (res){
+            console.log('deplay:', src);
+
+            src = 'deploy/' + src.replace(/^\/statics\/views\//, '');
+
+            res.pipe(safeWriteStream(src));
+          }).on('error', function (error){
+            console.log('problem with request:', error.message);
+          }).end();
+        }
+      });
+    });
+
+  });
+}
+
+read('statics/views');
