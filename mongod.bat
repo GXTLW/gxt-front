eade6e743a346ea1e66fc2cb869e28cc47c1c9c4
nuintun <nuintun@qq.com>
2016-09-18 11:48:14 +0800

    update files

--- mongod.bat
+++ mongod.bat
@@ -1,1 +1,1 @@
-D:/Program Files/MongoDB/Server/3.2/bin/mongod.exe -f E:/WORKSPACE/GitHub/gxt-front/mongod.conf
+"D:/Program Files/MongoDB/Server/3.2/bin/mongod.exe" -f "E:/WORKSPACE/GitHub/gxt-front/mongod.conf"
