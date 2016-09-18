554d9e887933bddc424317033b0a9207a2fab4b7
nuintun <nuintun@qq.com>
2016-09-18 15:36:21 +0800

    update files

--- server.js
+++ server.js
@@ -3,17 +3,22 @@
  */
 
 const koa = require('koa');
+const path = require('path');
 const etag = require('koa-etag');
-const send = require('koa-send');
-const route = require('koa-route');
+const router = require('koa-router');
 const mongoose = require('mongoose');
 const session = require('koa-session');
 const favicon = require('koa-favicon');
 const convert = require('koa-convert');
+const staticCache = require('koa-static-cache');
 const responseTime = require('koa-response-time');
 const conditionalGET = require('koa-conditional-get');
 
 const app = new koa();
+const route = router();
+const cwd = process.cwd();
+const maxAge = 365 * 24 * 60 * 60;
+const assets = path.join(cwd, 'assets');
 
 // mongoose.connect('mongodb://localhost/test');
 
@@ -24,14 +29,19 @@ app.use(convert(session(app)));
 app.use(conditionalGET());
 app.use(etag());
 app.use(favicon());
+app.use(convert(staticCache(assets, { maxAge: maxAge })));
 
-app.use(route.get('/', context =>{
+route.get('/', context =>{
   if (!context.session.login) {
     context.session.login = true;
   }
 
   context.body = 'hello';
-}));
+});
+
+app
+  .use(route.routes())
+  .use(router.allowedMethods());
 
 app.listen(8080, ()=>{
   console.log('listening on port 8080.');
