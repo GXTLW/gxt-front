53af2b1b83e607fbe999a9d580a8288d998ad795
nuintun <nuintun@qq.com>
2016-09-18 15:06:50 +0800

    update files

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
 /**
  * Created by nuintun on 2016/9/18.
  */
+
+const koa = require('koa');
+const etag = require('koa-etag');
+const send = require('koa-send');
+const route = require('koa-route');
+const mongoose = require('mongoose');
+const session = require('koa-session');
+const favicon = require('koa-favicon');
+const convert = require('koa-convert');
+const responseTime = require('koa-response-time');
+const conditionalGET = require('koa-conditional-get');
+
+const app = new koa();
+
+// mongoose.connect('mongodb://localhost/test');
+
+app.keys = ['GXT'];
+
+app.use(responseTime());
+app.use(convert(session(app)));
+app.use(conditionalGET());
+app.use(etag());
+app.use(favicon());
+
+app.use(route.get('/', context =>{
+  if (!context.session.login) {
+    context.session.login = true;
+  }
+
+  context.body = 'hello';
+}));
+
+app.listen(8080, ()=>{
+  console.log('listening on port 8080.');
+});
