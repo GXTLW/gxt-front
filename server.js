712ec05e78d9c6a9647508a32d182a091f8f16a5
nuintun <nuintun@qq.com>
2016-09-18 17:59:23 +0800

    update files

--- server.js
+++ server.js
@@ -4,11 +4,11 @@
 
 const koa = require('koa');
 const path = require('path');
+const send = require('koa-send');
 const router = require('koa-router');
 // const mongoose = require('mongoose');
 const session = require('koa-session');
 const convert = require('koa-convert');
-const staticCache = require('koa-static-cache');
 const responseTime = require('koa-response-time');
 
 const app = new koa();
@@ -19,25 +19,50 @@ const maxAge = 365 * 24 * 60 * 60;
 
 // mongoose.connect('mongodb://localhost/test');
 
+function serve(path, root, options){
+  // remove / begin
+  path = path.replace(/^\/+/, '');
+  options = options || {};
+  options.root = root || cwd;
+
+  return function (ctx, next){
+    if (ctx.method == 'HEAD' || ctx.method == 'GET') {
+
+      let req_path_array = ctx.path.slice(1).split('/');
+
+      // match path
+      if (path.length == 0 || path == req_path_array[0]) {
+        // if not serve the root
+        // then remove the filtered folder from path
+        if (path.length != 0) {
+          req_path_array = req_path_array.slice(1);
+        }
+
+        return send(ctx, req_path_array.join('/') || '/', options).then(() =>{
+          return next();
+        });
+      }
+    }
+
+    return next();
+  };
+}
+
 app.keys = ['GXT'];
 
 // response time
 app.use(responseTime());
-// assets
-app.use(convert(staticCache(path.join(cwd, assets), {
-  dynamic: true,
-  maxAge: maxAge,
-  prefix: assets
-})));
 // session
 app.use(convert(session(app)));
+// assets
+app.use(serve(assets, path.join(cwd, assets), { gzip: true }));
 
-route.get('/', context=>{
-  if (!context.session.login) {
-    context.session.login = true;
+route.get('/', ctx=>{
+  if (!ctx.session.login) {
+    ctx.session.login = true;
   }
 
-  context.body = 'hello';
+  ctx.body = 'hello';
 });
 
 app.use(route.routes());
