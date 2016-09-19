03fa49efe4485b73330651f4546c14ad7e72d264
nuintun <nuintun@qq.com>
2016-09-19 15:06:55 +0800

    update files

--- Assets/js/views/common.js
+++ Assets/js/views/common.js
@@ -3,3 +3,26 @@
  */
 
 'use strict';
+
+var $ = require('jquery');
+var Carousel = require('carousel');
+
+$(function (){
+  var banner = $('.ui-banner');
+  var panels = banner.find('.ui-slider li');
+
+  // 主广告
+  new Carousel({
+    element: banner,
+    panels: panels,
+    hasTriggers: true,
+    easing: 'easeOutStrong',
+    effect: 'scrollx',
+    classPrefix: 'ui-banner',
+    prevBtn: banner.find('.ui-banner-prev'),
+    nextBtn: banner.find('.ui-banner-next'),
+    viewSize: [1920, 400],
+    triggerType: 'click',
+    autoplay: true
+  }).render();
+});
