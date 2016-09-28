edcb16d5fd875c2e0f1a620e7830fb7ff8b63a91
nuintun <nuintun@qq.com>
2016-09-28 15:09:33 +0800

    update files

--- public/js/common/marquee/0.3.0/index.js
+++ public/js/common/marquee/0.3.0/index.js
@@ -10,83 +10,90 @@ var $ = require('./pause');
 $.fn.marquee = function (options){
   return this.each(function (){
     // Extend the options if any provided
-    var o = $.extend({}, $.fn.marquee.defaults, options),
-      $this = $(this),
-      $marqueeWrapper, containerWidth, animationCss, verticalDir, elWidth,
-      loopCount = 3,
-      playState = 'animation-play-state',
-      css3AnimationIsSupported = false,
+    var loopCount = 3;
+    var $this = $(this);
+    var css3AnimationIsSupported = false;
+    var playState = 'animation-play-state';
+    var o = $.extend({}, $.fn.marquee.defaults, options);
+    var $marqueeWrapper, containerWidth, animationCss, verticalDir, elWidth;
 
-      // Private methods
-      _prefixedEvent = function (element, type, callback){
-        var pfx = ['webkit', 'moz', 'MS', 'o', ""];
-        for (var p = 0; p < pfx.length; p++) {
-          if (!pfx[p]) type = type.toLowerCase();
-          element.addEventListener(pfx[p] + type, callback, false);
+    // Private methods
+    var _prefixedEvent = function (element, type, callback){
+      var pfx = ['webkit', 'moz', 'MS', 'o', ''];
+
+      for (var p = 0; p < pfx.length; p++) {
+        if (!pfx[p]) type = type.toLowerCase();
+
+        element.addEventListener(pfx[p] + type, callback, false);
+      }
+    };
+
+    var _objToString = function (obj){
+      var tabjson = [];
+
+      for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+          tabjson.push(p + ':' + obj[p]);
         }
-      },
+      }
 
-      _objToString = function (obj){
-        var tabjson = [];
-        for (var p in obj) {
-          if (obj.hasOwnProperty(p)) {
-            tabjson.push(p + ':' + obj[p]);
+      tabjson.push();
+
+      return '{' + tabjson.join(',') + '}';
+    };
+
+    var _startAnimationWithDelay = function (){
+      $this.timer = setTimeout(animate, o.delayBeforeStart);
+    };
+
+    // Public methods
+    var methods = {
+      pause: function (){
+        if (css3AnimationIsSupported && o.allowCss3Support) {
+          $marqueeWrapper.css(playState, 'paused');
+        } else {
+          // pause using pause plugin
+          if ($.fn.pause) {
+            $marqueeWrapper.pause();
           }
         }
-        tabjson.push();
-        return '{' + tabjson.join(',') + '}';
+
+        // save the status
+        $this.data('runningStatus', 'paused');
+        // fire event
+        $this.trigger('paused');
       },
 
-      _startAnimationWithDelay = function (){
-        $this.timer = setTimeout(animate, o.delayBeforeStart);
-      },
-
-      // Public methods
-      methods = {
-        pause: function (){
-          if (css3AnimationIsSupported && o.allowCss3Support) {
-            $marqueeWrapper.css(playState, 'paused');
-          } else {
-            // pause using pause plugin
-            if ($.fn.pause) {
-              $marqueeWrapper.pause();
-            }
+      resume: function (){
+        // resume using css3
+        if (css3AnimationIsSupported && o.allowCss3Support) {
+          $marqueeWrapper.css(playState, 'running');
+        } else {
+          // resume using pause plugin
+          if ($.fn.resume) {
+            $marqueeWrapper.resume();
           }
-          // save the status
-          $this.data('runningStatus', 'paused');
-          // fire event
-          $this.trigger('paused');
-        },
-
-        resume: function (){
-          // resume using css3
-          if (css3AnimationIsSupported && o.allowCss3Support) {
-            $marqueeWrapper.css(playState, 'running');
-          } else {
-            // resume using pause plugin
-            if ($.fn.resume) {
-              $marqueeWrapper.resume();
-            }
-          }
-          // save the status
-          $this.data('runningStatus', 'resumed');
-          // fire event
-          $this.trigger('resumed');
-        },
-
-        toggle: function (){
-          methods[$this.data('runningStatus') == 'resumed' ? 'pause' : 'resume']();
-        },
-
-        destroy: function (){
-          // Clear timer
-          clearTimeout($this.timer);
-          // Unbind all events
-          $this.find('*').addBack().unbind();
-          // Just unwrap the elements that has been added using this plugin
-          $this.html($this.find('.js-marquee:first').html());
         }
-      };
+
+        // save the status
+        $this.data('runningStatus', 'resumed');
+        // fire event
+        $this.trigger('resumed');
+      },
+
+      toggle: function (){
+        methods[$this.data('runningStatus') == 'resumed' ? 'pause' : 'resume']();
+      },
+
+      destroy: function (){
+        // Clear timer
+        clearTimeout($this.timer);
+        // Unbind all events
+        $this.find('*').addBack().unbind();
+        // Just unwrap the elements that has been added using this plugin
+        $this.html($this.find('.js-marquee:first').html());
+      }
+    };
 
     // Check for methods
     if (typeof options === 'string') {
@@ -124,6 +131,7 @@ $.fn.marquee = function (options){
             attr = false;
             break;
         }
+
         o[key] = attr;
       }
     });
@@ -159,6 +167,7 @@ $.fn.marquee = function (options){
     // If direction is up or down, get the height of main element
     if (verticalDir) {
       var containerHeight = $this.height();
+
       $marqueeWrapper.removeAttr('style');
       $this.height(containerHeight);
 
@@ -216,13 +225,12 @@ $.fn.marquee = function (options){
     }
 
     if (o.allowCss3Support) {
-      var
-        elm = document.body || document.createElement('div'),
-        animationName = 'marqueeAnimation-' + Math.floor(Math.random() * 10000000),
-        domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
-        animationString = 'animation',
-        animationCss3Str = '',
-        keyframeString = '';
+      var elm = document.body || document.createElement('div');
+      var animationName = 'marqueeAnimation-' + Math.floor(Math.random() * 10000000);
+      var domPrefixes = 'Webkit Moz O ms Khtml'.split(' ');
+      var animationString = 'animation';
+      var animationCss3Str = '';
+      var keyframeString = '';
 
       // Check css3 support
       if (elm.style.animation) {
@@ -234,6 +242,7 @@ $.fn.marquee = function (options){
         for (var i = 0; i < domPrefixes.length; i++) {
           if (elm.style[domPrefixes[i] + 'AnimationName'] !== undefined) {
             var prefix = '-' + domPrefixes[i].toLowerCase() + '-';
+
             animationString = prefix + animationString;
             playState = prefix + playState;
             keyframeString = '@' + prefix + 'keyframes ' + animationName + ' ';
@@ -250,11 +259,12 @@ $.fn.marquee = function (options){
     }
 
     var _rePositionVertically = function (){
-        $marqueeWrapper.css('margin-top', o.direction == 'up' ? containerHeight + 'px' : '-' + elHeight + 'px');
-      },
-      _rePositionHorizontally = function (){
-        $marqueeWrapper.css('margin-left', o.direction == 'left' ? containerWidth + 'px' : '-' + elWidth + 'px');
-      };
+      $marqueeWrapper.css('margin-top', o.direction == 'up' ? containerHeight + 'px' : '-' + elHeight + 'px');
+    };
+
+    var _rePositionHorizontally = function (){
+      $marqueeWrapper.css('margin-left', o.direction == 'left' ? containerWidth + 'px' : '-' + elWidth + 'px');
+    };
 
     // if duplicated option is set to true than position the wrapper
     if (o.duplicated) {
@@ -293,33 +303,37 @@ $.fn.marquee = function (options){
         // When duplicated, the first loop will be scroll longer so double the duration
         if (loopCount === 1) {
           o._originalDuration = o.duration;
+
           if (verticalDir) {
             o.duration = o.direction == 'up' ? o.duration + (containerHeight / ((elHeight) / o.duration)) : o.duration * 2;
           } else {
             o.duration = o.direction == 'left' ? o.duration + (containerWidth / ((elWidth) / o.duration)) : o.duration * 2;
           }
+
           // Adjust the css3 animation as well
           if (animationCss3Str) {
             animationCss3Str = animationName + ' ' + o.duration / 1000 + 's ' + o.delayBeforeStart / 1000 + 's ' + o.css3easing;
           }
+
           loopCount++;
         }
         // On 2nd loop things back to normal, normal duration for the rest of animations
         else if (loopCount === 2) {
           o.duration = o._originalDuration;
+
           // Adjust the css3 animation as well
           if (animationCss3Str) {
             animationName = animationName + '0';
             keyframeString = $.trim(keyframeString) + '0 ';
             animationCss3Str = animationName + ' ' + o.duration / 1000 + 's 0s infinite ' + o.css3easing;
           }
+
           loopCount++;
         }
       }
 
       if (verticalDir) {
         if (o.duplicated) {
-
           // Adjust the starting point of animation only when first loops finishes
           if (loopCount > 2) {
             $marqueeWrapper.css('margin-top', o.direction == 'up' ? 0 : '-' + elHeight + 'px');
@@ -335,30 +349,34 @@ $.fn.marquee = function (options){
             if (animationCss3Str) {
               animationCss3Str = animationName + ' ' + o.duration / 1000 + 's ' + o.delayBeforeStart / 1000 + 's ' + o.css3easing;
             }
+
             animationCss = {
               'margin-top': o.direction == 'up' ? '-' + elHeight + 'px' : containerHeight + 'px'
             };
+
             loopCount++;
           } else if (loopCount === 3) {
             // Set the duration for the animation that will run forever
             o.duration = o._completeDuration;
+
             // Adjust the css3 animation as well
             if (animationCss3Str) {
               animationName = animationName + '0';
               keyframeString = $.trim(keyframeString) + '0 ';
               animationCss3Str = animationName + ' ' + o.duration / 1000 + 's 0s infinite ' + o.css3easing;
             }
+
             _rePositionVertically();
           }
         } else {
           _rePositionVertically();
+
           animationCss = {
             'margin-top': o.direction == 'up' ? '-' + ($marqueeWrapper.height()) + 'px' : containerHeight + 'px'
           };
         }
       } else {
         if (o.duplicated) {
-
           // Adjust the starting point of animation only when first loops finishes
           if (loopCount > 2) {
             $marqueeWrapper.css('margin-left', o.direction == 'left' ? 0 : '-' + elWidth + 'px');
@@ -375,23 +393,28 @@ $.fn.marquee = function (options){
             if (animationCss3Str) {
               animationCss3Str = animationName + ' ' + o.duration / 1000 + 's ' + o.delayBeforeStart / 1000 + 's ' + o.css3easing;
             }
+
             animationCss = {
               'margin-left': o.direction == 'left' ? '-' + elWidth + 'px' : containerWidth + 'px'
             };
+
             loopCount++;
           } else if (loopCount === 3) {
             // Set the duration for the animation that will run forever
             o.duration = o._completeDuration;
+
             // Adjust the css3 animation as well
             if (animationCss3Str) {
               animationName = animationName + '0';
               keyframeString = $.trim(keyframeString) + '0 ';
               animationCss3Str = animationName + ' ' + o.duration / 1000 + 's 0s infinite ' + o.css3easing;
             }
+
             _rePositionHorizontally();
           }
         } else {
           _rePositionHorizontally();
+
           animationCss = {
             'margin-left': o.direction == 'left' ? '-' + elWidth + 'px' : containerWidth + 'px'
           };
@@ -405,8 +428,9 @@ $.fn.marquee = function (options){
       if (css3AnimationIsSupported) {
         // Add css3 animation to the element
         $marqueeWrapper.css(animationString, animationCss3Str);
-        var keyframeCss = keyframeString + ' { 100%  ' + _objToString(animationCss) + '}',
-          $styles = $marqueeWrapper.find('style');
+
+        var keyframeCss = keyframeString + ' { 100%  ' + _objToString(animationCss) + '}';
+        var $styles = $marqueeWrapper.find('style');
 
         // Now add the keyframe animation to the marquee element
         if ($styles.length !== 0) {
@@ -420,17 +444,18 @@ $.fn.marquee = function (options){
         _prefixedEvent($marqueeWrapper[0], 'AnimationIteration', function (){
           $this.trigger('finished');
         });
+
         // Animation stopped
         _prefixedEvent($marqueeWrapper[0], 'AnimationEnd', function (){
           animate();
           $this.trigger('finished');
         });
-
       } else {
         // Start animating
         $marqueeWrapper.animate(animationCss, o.duration, o.easing, function (){
           // fire event
           $this.trigger('finished');
+
           // animate again
           if (o.pauseOnCycle) {
             _startAnimationWithDelay();
@@ -439,6 +464,7 @@ $.fn.marquee = function (options){
           }
         });
       }
+      
       // save the status
       $this.data('runningStatus', 'resumed');
     };
