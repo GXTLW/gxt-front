95a584fc7438cdad64b78c1cce67c82afe0d3faa
nuintun <nuintun@qq.com>
2016-10-09 15:57:14 +0800

    update files

---
+++ spider.js
@@ -0,0 +1,32 @@
+/**
+ * Created by nuintun on 2016/10/9.
+ */
+
+'use strict';
+
+var urls = [
+  '',
+  '/about',
+  '/about/advantage',
+  '/about/aptitude',
+  '/about/culture',
+  '/about/history',
+  '/business',
+  '/business/insurance',
+  '/business/outsource',
+  '/business/proxy',
+  '/business/temporary',
+  '/contact',
+  '/contact/comment',
+  '/contact/school',
+  '/contact/society',
+  '/cooperation',
+  '/custom',
+  '/jobs',
+  '/news',
+  '/news/help',
+  '/news/policy',
+  '/news/trade',
+].map(function (value){
+  return 'https://gxt.herokuapp.com' + value;
+});
