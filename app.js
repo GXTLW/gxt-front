f6d67e38e6a2dbc0a6700f99d5cd2b00d009666d
nuintun <nuintun@qq.com>
2017-11-15 12:21:23 +0800

    update files

--- app.js
+++ app.js
@@ -19,21 +19,29 @@ const serve = require('./middlewares/serve');
 const intro = require('./middlewares/intro');
 const engine = require('./middlewares/engine');
 const responseTime = require('koa-response-time');
-const interceptors = require('koa-interceptors')();
+const Interceptors = require('koa-interceptors');
 
 const cwd = util.cwd;
 const app = new koa();
 const statics = 'public';
 const maxAge = 365 * 24 * 60 * 60;
+const interceptors = new Interceptors();
 
 if (process.env.NODE_ENV === 'local' || process.env.NODE_ENV === 'development') {
   // onerror
   require('koa-onerror')(app);
   // set engine
-  engine(app, { watch: true, root: config.view_base, extname: config.view_ext });
+  engine(app, {
+    watch: true,
+    root: config.view_base,
+    extname: config.view_ext
+  });
 } else {
   // set engine
-  engine(app, { root: config.view_base, extname: config.view_ext });
+  engine(app, {
+    root: config.view_base,
+    extname: config.view_ext
+  });
 }
 
 // mongoose.connect('mongodb://localhost/test');
@@ -44,7 +52,10 @@ app.keys = ['GXT', '8888168'];
 // response time
 app.use(responseTime());
 // session
-interceptors.use(convert(session(app, { key: 'GXT', maxAge: maxAge })));
+interceptors.use(convert(session(app, {
+  key: 'GXT',
+  maxAge: maxAge
+})));
 // resource
 interceptors.use(intro(config));
 // routers
@@ -56,13 +67,15 @@ app.use(favicon('favicon.ico'));
 if (util.env.development) {
   app.use(serve(statics, path.join(cwd, statics)));
 } else {
-  app.use(serve(statics, path.join(cwd, statics), { maxAge: maxAge }));
+  app.use(serve(statics, path.join(cwd, statics), {
+    maxAge: maxAge
+  }));
   // compress
   app.use(require('koa-compress')());
 }
 
 // error handler
-app.on('error', (error, ctx)=>{
+app.on('error', (error, ctx) => {
   // do nothing
 });
 
